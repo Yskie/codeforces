1e663fc9960413d38eda4d4c5491909daf7a88b7
Ysk <ysk@Ysks-MacBook-Pro.local>
2023-09-23 03:35:15 +0800

    first commit

---
+++ p71a.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class p71a {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        for (int i = 0; i < l; i++) {
+            String w = br.readLine();
+            int len = w.length();
+            if(len > 10) {
+                PrintWriter out = new PrintWriter(System.out);
+                out.print(w.charAt(0));
+                out.print(len - 2);
+                out.print(w.charAt(len - 1));
+                out.print("\n");
+                out.flush();
+            } else {
+                System.out.println(w);
+            }
+        }
+
+    }
+}
