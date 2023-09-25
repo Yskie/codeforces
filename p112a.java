2d040403302a5cc111b87d0de1cde1a620324266
Ysk <ysk@Ysks-MacBook-Pro.local>
2023-09-25 17:09:33 +0800

    adding new sol

--- p112a.java
+++ p112a.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 
 public class p112a {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1 = br.readLine();
-        String s2 = br.readLine();
+        String s1 = br.readLine().toLowerCase();
+        String s2 = br.readLine().toLowerCase();
 
-        System.out.println(s1.compareTo(s2));
+        int res = s1.compareTo(s2);
+        if (res > 0) {
+            System.out.println(1);
+        } else if (res < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(0);
+        }
+
     }
 }
