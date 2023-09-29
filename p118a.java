82b8c9ae00bcca59bf5d35e7624abf3f0f7e0106
Ysk <ysk@Ysks-MacBook-Pro.local>
2023-09-29 22:25:30 +0800

    adding new sol

--- p118a.java
+++ p118a.java
@@ -8,18 +8,14 @@ public class p118a {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] s = br.readLine().toLowerCase().toCharArray();
-        ArrayList<Character> a = new ArrayList<>();
+        PrintWriter out = new PrintWriter(System.out);
+
         for (int i = 0; i < s.length; i++) {
             char word = s[i];
-            if (word != 'a' && word != 'e' && word != 'i' && word != 'o' && word != 'u') {
-                a.add(word);
+            if (word != 'a' && word != 'e' && word != 'i' && word != 'o' && word != 'u' && word != 'y') {
+                out.print(".");    
+                out.print(word);
             }
-        }
-        
-        PrintWriter out = new PrintWriter(System.out);
-        for (int i = 0; i < a.size(); i++) {
-            out.print(".");    
-            out.print(a.get(i));
         }
         out.flush();
     }
