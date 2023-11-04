23ab8f1078549cfeb20a672b9ee483a8b00aaf8d
Ysk <ysk9680@gmail.com>
2023-11-05 04:30:55 +0800

    adding new solution

--- p469a.java
+++ p469a.java
@@ -6,15 +6,30 @@ public class p469a {
         PrintWriter out = new PrintWriter(System.out);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(br.readLine());
-        String[] s1 = br.readLine().split(" ");
-        String[] s2 = br.readLine().split(" ");
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int i = 0; i < s1.length; i++) {
+
+        String s = br.readLine();
+        String[] s1;
+        String[] s2;
+        if (s.length() == 0) {
+            s1 = new String[0];
+        } else {
+            s1 = s.split(" ");
+        }
+
+        s = br.readLine();
+        if (s.length() == 0) {
+            s2 = new String[0];
+        } else {
+            s2 = s.split(" ");
+        }
+                        
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 1; i < s1.length; i++) {
             set.add(Integer.parseInt(s1[i]));
         }
-        for (int i = 0; i < s2.length; i++) {
+        for (int i = 1; i < s2.length; i++) {
             set.add(Integer.parseInt(s2[i]));
-        }        
+        }
 
         for (int i = 1; i <= l; i++) {
             if (!set.contains(i)) {
@@ -25,5 +40,5 @@ public class p469a {
 
         out.println("I become the guy.");
         out.flush();
-    }   
+    }
 }
