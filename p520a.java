56695a6a15918a36009b73289dbb2c42b0c1082c
Ysk <ysk9680@gmail.com>
2023-11-09 01:13:32 +0800

    adding new solution

--- p520a.java
+++ p520a.java
@@ -7,16 +7,19 @@ public class p520a {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(br.readLine());
         char[] l2 = br.readLine().toLowerCase().toCharArray();
-        char[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
-    't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        Character[] data = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        HashSet<Character> hs = new HashSet<>();
+        for (int i = 0; i < data.length; i++) {
+            hs.add(data[i]);
+        }
 
-        List<char[]> a = Arrays.asList(data);
-        System.out.println(a.get(1));
-
-        
-
-
-
+        for (int i = 0; i < l; i++) {
+            if (hs.contains(l2[i])) {
+                hs.remove(l2[i]);
+            }
+        }
+        out.println(hs.isEmpty() ? "YES" : "NO");
         out.flush();
-    }   
+    }
 }
