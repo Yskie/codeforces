875fe72e5247f3c5ceee155710110bb9cf63dbc4
Ysk <ysk9680@gmail.com>
2023-12-07 08:12:40 +0800

    adding new solution

---
+++ p451a.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class p541a {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" "); 
+        int s1 = Integer.parseInt(s[0]);
+        int s2 = Integer.parseInt(s[1]);
+        int min = Math.min(s1, s2);
+
+        if (min % 2 == 0) {
+            out.println("Malvika");
+        } else {
+            out.printlin("Akshat");
+        }
+        
+        out.flush();
+    }
+}
