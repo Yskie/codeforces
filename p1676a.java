928b5f563a896fb91d36bcb349080dc7278a9555
Ysk <ysk9680@gmail.com>
2023-12-24 20:56:17 +0800

    adding new solution

---
+++ p1676a.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class p1676a {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        for (int i = 0; i < l; i++) {
+            char[] c = br.readLine().toCharArray();
+            int a = c[0] + c[1] + c[2];
+            int b = c[3] + c[4] + c[5];
+            if (a == b) out.println("YES");
+            else out.println("NO");
+        }
+        out.flush();
+    }
+}
