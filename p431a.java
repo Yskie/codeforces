50ffe71c3c7ae7a671e9e79174d40a455b79669c
Ysk <ysk9680@gmail.com>
2024-01-04 12:29:41 +0800

    adding new solution

---
+++ p431a.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class p431a {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int s1 = Integer.parseInt(s[0]);
+        int s2 = Integer.parseInt(s[1]);
+        int s3 = Integer.parseInt(s[2]);
+        int s4 = Integer.parseInt(s[3]);
+        int o = 0;
+        char[] k = br.readLine().toCharArray();
+        for (int i = 0; i < k.length; i++) {
+            char c = k[i];
+            if (c == '1') {
+                o += s1;
+            }
+
+            if (c == '2') {
+                o += s2;
+            }
+            
+            if (c == '3') {
+                o += s3;
+            }
+            
+            if (c == '4') {
+                o += s4;
+            }            
+        }
+        out.println(o);
+        out.flush();
+    }
+}
