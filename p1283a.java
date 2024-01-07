f3bdc52a7a3e1cc48db7a2a39c68505531a9ee22
Ysk <ysk9680@gmail.com>
2024-01-07 11:59:09 +0800

    adding new solution

---
+++ p1283a.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class p1283a {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        for (int i = 0; i < l; i++) {
+            String[] s = br.readLine().split(" ");
+            int s1 = Integer.parseInt(s[0]);
+            int s2 = Integer.parseInt(s[1]);
+            int org = 60 - s2;
+            org += (24 - s1 - 1) * 60;
+            out.println(org);
+        }
+
+        out.flush();
+    }
+}
