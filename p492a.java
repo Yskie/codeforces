c96f1719e2c3644a47fda9cb64bd47fe9b9db7ce
Ysk <ysk9680@gmail.com>
2024-01-13 01:13:42 +0800

    adding new solution

--- p492a.java
+++ p492a.java
@@ -5,30 +5,22 @@ public class p492a {
     public static void main(String[] args) throws IOException {
         PrintWriter out = new PrintWriter(System.out);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" "); 
-        int s1 = Integer.parseInt(s[0]);
-        int s2 = Integer.parseInt(s[1]);
-        String[] lan = br.readLine().split(" ");
-        ArrayList<Double> ord = new ArrayList<>();
-        double output = 0;
-        for (int i = 0; i < s1; i++) {
-            ord.add(Double.parseDouble(lan[i]));
-        }
+        int l = Integer.parseInt(br.readLine());
 
-        Collections.sort(ord);
-
-        for (int i = 0; i < s1 - 1; i++) {                        
-            double diff = (ord.get(i + 1) - ord.get(i)) / 2;
-            if (diff > output) output = diff;
-        }
-
-        //Get the start and end.
-        double sDiff = (ord.get(0));
-        double eDiff = (s2 - ord.get(s1 - 1));
-
-        if (sDiff > output) output = sDiff;
-        if (eDiff > output) output = eDiff;
-        System.out.println(output);
+        int c = 0;
+        int r = 0;
+        int prev = 0;
+        int o = 0;
+        while (o < l) {
+            c++;
+            r += c;
+            o += r;
+            if (o > l) {
+                c--;
+                break;
+            }
+        }        
+        out.println(c);
         out.flush();
     }
 }
